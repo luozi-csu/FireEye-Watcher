3baaa77af0e1fc3599f0001b795db3c0dcb4ed7e
luozi-csu <2848411850@qq.com>
2023-04-26 00:12:32 +0800

    [add] posix

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 from flask import Flask
 from middleware import Middleware
@@ -13,4 +14,9 @@ app.register_blueprint(file_controller)
 app.register_blueprint(record_controller)
 
 if __name__ == "__main__":
+    if os.name == "posix":
+        import signal
+        def handle_sigchld(*args):
+            pid, status = os.waitpid(-1, os.WNOHANG)
+        signal.signal(signal.SIGCHLD, handle_sigchld)
     app.run(host=config["host"], port=config["port"])
