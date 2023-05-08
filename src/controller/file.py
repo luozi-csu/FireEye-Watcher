0de11a7070285b91f3fe047814fdf3a473df5612
luozi <2848411850@qq.com>
2023-05-08 15:55:11 +0800

    [add] video

--- src/controller/file.py
+++ src/controller/file.py
@@ -1,7 +1,10 @@
+import os
+import sys
 import time
+import shutil
 from log import logger
-from flask import Blueprint, request
-from common import response_success
+from flask import Blueprint, request, send_file
+from common import response_success, response_failed
 from model import Record
 from service import RecordService
 from repository import RecordRepository
@@ -9,6 +12,13 @@ from database import db
 from algorithm.yolov5 import detect, classify
 from multiprocessing import Process
 from util import *
+from pathlib import Path
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))
 
 record_repository = RecordRepository(conn=db)
 record_service = RecordService(record_repo=record_repository)
@@ -32,12 +42,15 @@ def process_video(uid, path):
     record.result = res
     record.finished_time = int(time.time())
     record_service.update_record(record.id, uid, record)
+    # move processed video
+    video_path = "algorithm/yolov5/runs/detect/exp" + "/" + path.split("/")[-1]
+    shutil.move(video_path, "/var/upload/fireeye/processed/{id}.mp4".format(id=record.id))
     logger.debug("update record successfully")
 
 @file_controller.post("/upload")
 def upload_file():
     f = request.files['upload']
-    path = "/var/upload/fireeye/" + f.filename
+    path = "/var/upload/fireeye/origin/" + f.filename
     f.save(path)
 
     token = request.headers.get("Authorization")
@@ -51,3 +64,15 @@ def upload_file():
     p.start()
 
     return response_success(record.to_json(), "upload video successfully")
+
+@file_controller.get("/videos/<int:id>")
+def get_video(id):
+    record = record_service.get_record(id)
+    if record == None:
+        return response_failed(404, "record not found")
+    
+    video_path = "/var/upload/fireeye/processed" + "/" + "{id}.mp4".format(id=id)
+    if not os.path.exists(video_path):
+        return response_failed(404, "video file not found")
+    
+    return send_file(video_path)
---
+++ ui/app/src/main/java/com/luozi/fireeyewatcher/activity/VideoActivity.java
@@ -0,0 +1,16 @@
+package com.luozi.fireeyewatcher.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.luozi.fireeyewatcher.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
